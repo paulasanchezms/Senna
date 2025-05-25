e08bfff2eb69670b51442238f3c3fe0a750ad36f
paulasanchezms <paulasm01@gmail.com>
2025-05-25 17:16:01 +0200

    hora

--- src/main/java/com/senna/senna/DTO/WorkingHourCustomDTO.java
+++ src/main/java/com/senna/senna/DTO/WorkingHourCustomDTO.java
@@ -6,7 +6,7 @@ import lombok.Data;
 public class WorkingHourCustomDTO {
     private Long id;
     private Long profileId;
-    private String date; // formato ISO '2025-05-21'
-    private String startTime; // 'HH:mm'
-    private String endTime;   // 'HH:mm'
+    private String date;
+    private String startTime;
+    private String endTime;
 }
